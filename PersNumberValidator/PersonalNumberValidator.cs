970f4928ba8467073bd1b61f4a9286ac7e5d3cd6
Daniel Nejdmo <dnejdmo@hotmail.com>
2025-01-10 15:09:20 +0100

    initial commit

---
+++ PersNumberValidator/PersonalNumberValidator.cs
@@ -0,0 +1,9 @@
+﻿public class PersonalNumberValidator
+{
+    //tar bort eventuella bindestreck och mellanslag
+    public string CleanPersonalNumber(string persNumber)
+    {
+        return persNumber.Replace("-", "").Replace(" ", "");
+    }
+
+}
---
+++ PersNumberValidator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Skapa en instans av PersonalNumberValidator
+        PersonalNumberValidator validator = new PersonalNumberValidator();
+
+        // Testa CleanPersonalNumber-metoden
+        Console.Write("Skriv ett personnummer: ");
+        string input = Console.ReadLine();
+
+        string cleanedPnr = validator.CleanPersonalNumber(input);
+
+        Console.WriteLine($"Rensat personnummer: {cleanedPnr}");
+    }
+}
