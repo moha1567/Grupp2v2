953175162e2aa8f4ccc98d1cb94efd3fcafe562e
Daniel Nejdmo <dnejdmo@hotmail.com>
2025-01-13 12:40:38 +0100

    Uppdaterat med en while och Clear i mainmetoden

--- PersNumberValidator/Program.cs
+++ PersNumberValidator/Program.cs
@@ -5,17 +5,29 @@
         // Skapa en instans av PersonalNumberValidator
         PersonalNumberValidator validator = new PersonalNumberValidator();
 
-        Console.Write("Skriv ett personnummer: ");
-        string input = Console.ReadLine();
+        while (true)
+        {
+            Console.Clear();
 
-        // Validera personnumret med en enda metod
-        if (validator.ValidatePersonalNumber(input))
-        {
-            Console.WriteLine("Personnumret är giltigt!");
-        }
-        else
-        {
-            Console.WriteLine("Ogiltigt personnummer!");
+            Console.Write("Skriv ett personnummer (eller tryck \"x\" för att avsluta): ");
+            string input = Console.ReadLine();
+
+            if (input.ToLower() == "x")
+            {
+                Console.WriteLine("Programmet avslutas. Tack för att du använde personnummervalideraren!");
+                break;
+            }
+            // Validera personnumret med en enda metod
+            if (validator.ValidatePersonalNumber(input))
+            {
+                Console.WriteLine("Personnumret är giltigt!");
+            }
+            else
+            {
+                Console.WriteLine("Ogiltigt personnummer!");
+            }
+            Console.WriteLine("\nTryck på valfri tangent för att fortsätta...");
+            Console.ReadKey();
         }
     }
 }
