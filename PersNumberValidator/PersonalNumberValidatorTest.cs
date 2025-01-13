690a7d050a3ecc0757d2f83a75a0785873c13275
Kevin BJ <kevin.b.jonsson@hotmail.com>
2025-01-13 15:58:40 +0100

    Added more test in "PersonalNumberValidatorTest"

--- PersNumberValidator/PersonalNumberValidatorTest.cs
+++ PersNumberValidator/PersonalNumberValidatorTest.cs
@@ -8,10 +8,59 @@ public class PersonalNumberValidatorTest
         _validator = new PersonalNumberValidator();
     }
     [Fact]
-    public void ClearPersNum_Empty()
+    public void CleanPersNum_Empty()
     {
         string input = "";
         string cleanedPnr = _validator.CleanPersonalNumber(input);
         Assert.Equal("", cleanedPnr);
+    }
+    [Fact]
+    public void CleanPersNum_Space()
+    {
+        string input = "   ";
+        string cleanedPnr = _validator.CleanPersonalNumber(input);
+        Assert.Equal("", cleanedPnr);
+    }
+    [Fact]
+    public void CleanPersNum_MixedCharacters()
+    {
+        string input = "1234-56X78#90";
+        string cleanedPnr = _validator.CleanPersonalNumber(input);
+        Assert.Equal("1234567890", cleanedPnr);
+    }
+    [Fact]
+    public void CleanPersNum_LongString() 
+    {
+        string input = "1234-56-78-90-abcdefg";
+        string cleanedPnr = _validator.CleanPersonalNumber(input);
+        Assert.Equal("1234567890", cleanedPnr);
+    }
+    [Fact]
+    public void CleanPersNum_ShortString()
+    {
+        string input = "1234-abcd";
+        string cleanedPnr = _validator.CleanPersonalNumber(input);
+        Assert.Equal("1234567890", cleanedPnr);
+    }
+    [Fact]
+    public void CleanPersNum_SingleCharacter()
+    {
+        string input = "A";
+        string cleanedPnr = _validator.CleanPersonalNumber(input);
+        Assert.Equal("", cleanedPnr);
+    }
+    [Fact]
+    public void CleanPersNum_PunctuatedInput()
+    {
+        string input = "1234-56-78,90!";
+        string cleanedPnr = _validator.CleanPersonalNumber(input);
+        Assert.Equal("1234567890", cleanedPnr);
+    }
+    [Fact]
+    public void CleanPersNum_SeveralLeadingAndSpaces()
+    {
+        string input = "   1234-56-78-90   ";
+        string cleanedPnr = _validator.CleanPersonalNumber(input);
+        Assert.Equal("1234567890", cleanedPnr);
     }
 }
